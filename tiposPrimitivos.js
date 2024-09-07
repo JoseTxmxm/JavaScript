b2939f994fd3142010f61311118bce74fa45a3ac
JoseTxmxm <joseortiztorres41@gmail.com>
2024-09-07 13:16:34 -0600

    Commit 3: Se agregó tiposPrimitivos.js al repositorio en la rama js_practice

---
+++ tiposPrimitivos.js
@@ -0,0 +1,27 @@
+/*
+*TODO: script que declare variables de cada tipo y muestre su tipo usando typeof.
+*/
+
+var num = 10;
+console.log(typeof num);
+
+var str = "Hello, World!";
+console.log(typeof str);
+
+var bool = true;
+console.log(typeof bool);
+
+var undefined;
+console.log(typeof undefined);
+
+var simbolo = Symbol("simbolo");
+console.log(typeof simbolo);
+
+var bigint = BigInt("132246548465135646546487688")
+console.log(typeof bigint);
+
+var st = null;
+console.log(typeof st);
+
+var st2 = null;
+console.log(st2);  
