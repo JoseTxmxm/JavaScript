259c14a8ab1506b433f211afe4feb5e52f71e4b0
JoseTxmxm <joseortiztorres41@gmail.com>
2024-09-07 13:15:48 -0600

    Commit 2: Se agregó arrayNumeros.js al repositorio en la rama js_practice

---
+++ arrayNumeros.js
@@ -0,0 +1,15 @@
+/*
+*TODO: Crear un array de números del 1 al 10 y muestre en cada número en la consola.
+*/
+
+const firstName = "jordan";
+const lastName = 'Hayashi'
+{
+    console.log(firstName, lastName)
+}
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
