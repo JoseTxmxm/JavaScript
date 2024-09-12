12ac6b769f73241e8039cb580c542d5d7fcdaf73
JoseTxmxm <joseortiztorres41@gmail.com>
2024-09-12 11:25:22 -0600

    Commit 4: Se modificó tiposPrimitovs.js y se agregó 2-objects.js al repositorio

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -2,26 +2,26 @@
 *TODO: script que declare variables de cada tipo y muestre su tipo usando typeof.
 */
 
-var num = 10;
+var num = 10;// var num muestra el tipo numero
 console.log(typeof num);
 
-var str = "Hello, World!";
+var str = "Hello, World!";// var str muestra el tipo string
 console.log(typeof str);
 
-var bool = true;
+var bool = true;// var bool muestra el tipo boolean
 console.log(typeof bool);
 
-var undefined;
+var undefined;// var undefined muestra el tipo undefined
 console.log(typeof undefined);
 
-var simbolo = Symbol("simbolo");
+var simbolo = Symbol("simbolo");// var simbolo muestra el tipo symbol
 console.log(typeof simbolo);
 
-var bigint = BigInt("132246548465135646546487688")
+var bigint = BigInt("132246548465135646546487688")// var bigint muestra el tipo bigint
 console.log(typeof bigint);
 
-var st = null;
+var st = null;// var st muestra el tipo objeto
 console.log(typeof st);
 
-var st2 = null;
+var st2 = null;// var st2 muestra el tipo null
 console.log(st2);  
